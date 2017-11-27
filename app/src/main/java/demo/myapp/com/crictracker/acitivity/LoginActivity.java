633e5e4251d9adc78f0b513f2ea8622896edadb7
eghambaram <calltomeegha@gmail.com>
2017-11-27 19:48:35 +0530

    ios commit

--- app/src/main/java/demo/myapp/com/crictracker/acitivity/LoginActivity.java
+++ app/src/main/java/demo/myapp/com/crictracker/acitivity/LoginActivity.java
@@ -31,7 +31,7 @@ public class LoginActivity extends AppCompatActivity
 
     Button buttonSignIn;
     Dialog myDialog;
-    /* test*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
